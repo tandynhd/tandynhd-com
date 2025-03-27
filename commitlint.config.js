818d9a42403bbdfb4d6a332f9564c317140cbbab
tandynhd <tandynhd@gmail.com>
2025-03-28 01:37:34 +0700

    feat: add commit lint

---
+++ commitlint.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat", // New feature
+        "fix", // Bug fix
+        "docs", // Documentation
+        "style", // Formatting, missing semi colons, etc
+        "refactor", // Code change that neither fixes a bug nor adds a feature
+        "perf", // Performance improvements
+        "test", // Adding tests
+        "chore", // Maintenance tasks
+        "revert", // Reverting changes
+        "ci", // CI/CD related
+        "build", // Build system or external dependencies
+      ],
+    ],
+    "type-case": [2, "always", "lowerCase"],
+    "type-empty": [2, "never"],
+    "scope-case": [2, "always", "lowerCase"],
+    "subject-case": [2, "always", "lowerCase"],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "header-max-length": [2, "always", 72],
+  },
+};
